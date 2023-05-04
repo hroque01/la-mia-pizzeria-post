48af15e73b04101b56a2036b7806edd2a60afdd6
Hadson Roque <nineonezero.light@gmail.com>
2023-05-04 15:24:21 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        List<Pizza> pizze = new List<Pizza>
+            {
+                new Pizza(1, "Pizza Margherita", "Bella buona", "https://www.nicesurgelati.it/wp-content/uploads/2018/05/pizza_margherita_nice.jpg", 6),
+                new Pizza(2, "Pizza Diavola", "Bella piccante bellabuona", "https://www.kennedyfood.it/wp-content/uploads/2019/03/diavola.jpg", 8),
+                new Pizza(3, "Pizza Bufala", "Buon pizza con mozzarella", "https://www.atuttofarro.it/wp-content/uploads/2019/02/atuttofarro_pizzarossa_bufala.jpg", 8),
+            };
+
+        public IActionResult Index()
+        { 
+            return View(pizze);
+        }
+
+        public IActionResult Details(int id) 
+        { 
+            var pizza = pizze.FirstOrDefault(p => p.Id == id);
+
+            if (pizza == null || id < 1 || id > pizze.Count)
+            {
+                return View("ErrorPizza");
+            }
+            
+            return View(pizza);
+        }
+
+    }
+}
---
+++ la-mia-pizzeria-static/Pizza.cs
@@ -0,0 +1,20 @@
+﻿namespace la_mia_pizzeria_static
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }  
+
+        public Pizza(int id, string name, string description, string image, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
